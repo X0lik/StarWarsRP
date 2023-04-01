57832fe0f42ba8e6f0f47f86f3ec283e48305d1c
X0lik <80484098+X0lik@users.noreply.github.com>
2023-04-01 23:24:37 +0300

    Edit player initializating & config sychronization

--- gamemodes/xilius/config.lua
+++ gamemodes/xilius/config.lua
@@ -5,10 +5,26 @@ XL.Modules["teams"] = true
 XL.Modules["cmd"] = true
 XL.Modules["resource"] = false
 XL.Modules["money"] = true
+XL.Modules["admin"] = true
+XL.Modules["http"] = false
+
 
 XL.Config = {}
-XL.Config.DefaultWalk = 160
-XL.Config.DefaultRun = 230
+XL.Config.DefaultCrouch = .4
+XL.Config.DefaultSlowWalk = 80
+XL.Config.DefaultWalk = 185
+XL.Config.DefaultRun = 250
 XL.Config.DefaultJump = 160
 
 XL.Config.StartMoney = 1000
+
+
+XL.Config.GiveAdminWeapons = true
+XL.Config.AdminWeapons = {
+	"weapon_physgun",
+	"gmod_tool"
+}
+
+XL.Config.HTTPStatus = false
+XL.Config.HTTPConnections = true
+XL.Config.HTTPAdminConnections = true
--- gamemodes/xilius/lib/player/sv_player_initializating.lua
+++ gamemodes/xilius/lib/player/sv_player_initializating.lua
@@ -1,7 +1,11 @@
 hook.Add( "PlayerInitialSpawn", "XL:SetDefaultTeam", function( ply )
 	timer.Simple( 0, function()
+
+		ply:SetCrouchedWalkSpeed( XL.Config.DefaultCrouch )
+		ply:SetSlowWalkSpeed( XL.Config.DefaultSlowJump )
 		ply:SetWalkSpeed( XL.Config.DefaultWalk )
 		ply:SetRunSpeed( XL.Config.DefaultRun )
+		ply:SetMaxSpeed( XL.Config.DefaultRun )
 		ply:SetJumpPower( XL.Config.DefaultJump )
 
 		if XL.Modules["money"] then
