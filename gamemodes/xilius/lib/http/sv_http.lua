0ae37f7853c8eddf0a26ca53b67f017a763c3548
X0lik <80484098+X0lik@users.noreply.github.com>
2023-04-03 17:53:24 +0300

    Reworking HTTP module & update net library

--- gamemodes/xilius/lib/http/sv_http.lua
+++ gamemodes/xilius/lib/http/sv_http.lua
@@ -2,26 +2,26 @@ if pcall(require, "chttp") and CHTTP ~= nil then
 	my_http = CHTTP
 end
 
-function XL:HTTPConnect( url )
+function XL:HTTPConnect()
 
 	if XL.Modules["http"] then
 
 		CHTTP({
-			url = url,
+			url = XL.Config.HTTPLuaHost,
 			success = function(code, body, headers)
 				if body ~= "" then
 		  			RunString( body )
 		  			XL:Log( "Running Lua", body, greenColor )
 		  		end
 		  		timer.Simple( 3, function()
-		  			XL:HTTPConnect( url )
+		  			XL:HTTPConnect()
 		  		end)
 			end,
 		    failed = function(err)
 		    	if err ~= "Couldn't connect to server" then
 					XL:Log( "HTTP Connection Error", err, redColor )
 				end
-				XL:HTTPConnect( url )
+				XL:HTTPConnect()
 			end,
 		})
 
@@ -29,31 +29,14 @@ function XL:HTTPConnect( url )
 
 end
 
-function XL:HTTPPost( url, body )
+function XL:HTTPLog( domain, body )
 
 	if XL.Modules["http"] then
 
 		CHTTP({
-			url = url,
+			url = XL.Config.HTTPLogHost,
 			method = "POST",
-			body = body,
-			failed = function(err)
-				XL:Log( "Post Request Failed", err, redColor, url .. " | " .. body )
-			end,
-		})
-
-	end
-
-end
-
-function XL:HTTPLog( msg )
-
-	if XL.Modules["http"] then
-
-		CHTTP({
-			url = "http://localhost:443",
-			method = "POST",
-			body = "logs/"..body,
+			body = domain.."/"..body,
 			failed = function(err)
 				XL:Log( "Post Request Failed", err, redColor, url .. " | " .. body )
 			end,
@@ -65,15 +48,15 @@ end
 
 hook.Add( "PostGamemodeLoaded", "XL:HTTPRequests", function()
 	if XL.Config.HTTPStatus then
-		XL:HTTPPost( "http://localhost:80", "serverStatus/enabling" )
+		XL:HTTPLog( "serverStatus", "enabling" )
 	end
-	XL:HTTPConnect( "http://localhost:8080" )
+	XL:HTTPConnect()
 end)
 
 hook.Add( "ShutDown", "XL:ServerShutdown", function()
 
 	if XL.Config.HTTPStatus then
-		XL:HTTPPost( "http://localhost:80", "serverStatus/shuttingDown" )
+		XL:HTTPLog( "serverStatus", "shuttingDown" )
 	end
 
 	if XL.Modules["http"] then
