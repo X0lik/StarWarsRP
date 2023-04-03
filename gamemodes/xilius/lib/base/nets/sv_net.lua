a96a2f47ecbb56e60f669e77ca343441b5cd501e
X0lik <80484098+X0lik@users.noreply.github.com>
2023-04-03 17:17:11 +0300

    Rewriting net extensions
    Added new net method and set net cooldown

---
+++ gamemodes/xilius/lib/base/nets/sv_net.lua
@@ -0,0 +1,39 @@
+nw = {}
+function nw.Receive( name, timeout, func )
+
+    net.Receivers[name:lower()] = function( len, ply )
+        if nw.IsTimedOut( ply, name ) then return end
+
+        if func == nil then
+            timeout( len, ply )
+            nw.Timeout( ply, name, .1 )
+        else
+            func( len, ply )
+            nw.Timeout( ply, name, timeout )
+        end
+    end
+
+end
+_G.net.Receive = nw.Receive
+
+function nw.Timeout(ply, name, time)
+    ply.NetTimedOut[name] = true
+    timer.Simple( time, function()
+        if IsValid( ply ) then ply.NetTimedOut[name] = false end
+    end)
+end
+
+function nw.IsTimedOut(ply, name)
+    if ply.NetTimedOut[name] ~= nil and ply.NetTimedOut[name] then
+        XL:Log( ply:Name() .. " (" .. ply:SteamID() .. ") trying to use exploit", name, redColor )
+        return true
+    end
+    return false
+end
+
+--[[util.AddNetworkString( "TestNet" )
+net.Receive( "TestNet", 5, function(_,ply) 
+
+    print(ply)
+
+end)]]
