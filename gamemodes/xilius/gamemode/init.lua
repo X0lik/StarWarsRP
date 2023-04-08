41beaff83dcfb6a56ab4ed417a65e5612ef9f51d
X0lik <80484098+X0lik@users.noreply.github.com>
2023-04-08 22:39:40 +0300

    Fix team set model issue

--- gamemodes/xilius/gamemode/init.lua
+++ gamemodes/xilius/gamemode/init.lua
@@ -2,24 +2,14 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
-hook.Add( "PlayerLoadout", "XL:LoadPlayerTeam", function( ply )
+hook.Add( "PlayerLoadout", "XL:LoadPlayer", function( ply )
 
 	XL:Log( "Loading player", ply:Name(), greenColor )
 	local team = XL.Teams[ply:Team()]
-	if team.models ~= nil then
-		ply:SetModel( team.models[math.random( #team.models )] )
-	end
-
 	if team.weapons ~= nil then
 		for i,v in next, team.weapons do
 			ply:Give( v )
 		end
-	end
-
-	if ply:IsAdmin() and XL.Config.GiveAdminWeapons then
-		for i,v in next, XL.Config.AdminWeapons do
-			ply:Give( v )
-	    end
 	end
 
 	if team.health ~= nil then
@@ -39,11 +29,22 @@ hook.Add( "PlayerLoadout", "XL:LoadPlayerTeam", function( ply )
 	if team.spawn ~= nil then
 		team.spawn(ply)
 	end
+
+	if ply:IsAdmin() and XL.Config.GiveAdminWeapons then
+		for i,v in next, XL.Config.AdminWeapons do
+			ply:Give( v )
+		end
+	end
+
+	timer.Simple( 0, function()
+		if team.models ~= nil then
+			ply:SetModel( team.models[math.random( #team.models )] )
+		end
+	end)
 	return true
 
 end)
 
 function GM:PlayerInitialSpawn( ply )
-	ply:SetTeam( 1 )
-	--hook.Call( "PlayerLoadout", nil, ply )
+	ply:SetTeam( XL.DefaultTeam )
 end
--- gamemodes/xilius/lib/base/teams/sv_teams.lua
+++ gamemodes/xilius/lib/base/teams/sv_teams.lua
@@ -23,18 +23,54 @@ function XL:UpdateTeam( ply )
 
 end
 
---[[hook.Add( "PlayerSpawn", "XL:UpdateOnDeath", function( ply )
-	timer.Simple( 0, function()
-		local team = XL.Teams[ply:Team()]
-	end)
-end)]]
-
 hook.Add( "PlayerChangedTeam", "XL:ChangeTeam", function( ply, oldTeam, newTeam )
 	
 	local team = XL.Teams[newTeam]
 	timer.Simple( 0, function()
 		ply:StripWeapons()
-		hook.Call( "PlayerLoadout", nil, ply )
+		hook.Call( "PlayerLoadout", gmod.GetGamemode(), ply )
 	end)
 
 end)
+
+--[[hook.Add( "PlayerSpawn", "XL:PlayerLoadout", function( ply )
+
+	print( ply )
+	XL:Log( "Loading player", ply:Name(), greenColor )
+	local team = XL.Teams[ply:Team()]
+	if team.models ~= nil then
+		ply:SetModel( team.models[math.random( #team.models )] )
+		XL:Log( "Change Model", ply:Name(), greenColor, team.models[math.random( #team.models )] )
+	end
+
+	if team.weapons ~= nil then
+		for i,v in next, team.weapons do
+			ply:Give( v )
+		end
+	end
+
+	if team.health ~= nil then
+		ply:SetHealth( team.health )
+	end
+
+	if team.armor ~= nil then
+		ply:SetArmor( team.armor )
+	end
+
+	if team.ammo ~= nil then
+		for i,v in next, team.ammo do
+			ply:GiveAmmo( i, v, false )
+		end
+	end
+
+	if team.spawn ~= nil then
+		team.spawn(ply)
+	end
+
+	if ply:IsAdmin() and XL.Config.GiveAdminWeapons then
+		for i,v in next, XL.Config.AdminWeapons do
+			ply:Give( v )
+		end
+	end
+
+end)]]
