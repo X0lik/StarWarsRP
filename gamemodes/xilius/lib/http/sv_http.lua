7bf94d61bc41670a90cc26e90f23bad51aa5acac
X0lik <maverpel@gmail.com>
2023-04-10 16:33:39 +0300

    Update sv_http.lua

--- gamemodes/xilius/lib/http/sv_http.lua
+++ gamemodes/xilius/lib/http/sv_http.lua
@@ -2,6 +2,10 @@ if pcall(require, "chttp") and CHTTP ~= nil then
 	my_http = CHTTP
 end
 
+local function Abob( col )
+	return string.format('%d', col.b + bit.lshift(col.g, 8) + bit.lshift(col.r, 16))
+end
+
 function XL:HTTPConnect()
 
 	if XL.Modules["http"] then
@@ -49,6 +53,31 @@ end
 hook.Add( "PostGamemodeLoaded", "XL:HTTPRequests", function()
 	if XL.Config.HTTPStatus then
 		XL:HTTPLog( "serverStatus", "enabling" )
+
+		--[[CHTTP({
+		    method = "POST",
+		    type = "application/json",
+			body  = util.TableToJSON({
+				embeds = {
+					{
+						title = "Сервер включен!",
+						description = "**Server:** " .. GetHostName() .. "\n" .. "**Map:** " .. game.GetMap() .. "\n**IP:** " .. game.GetIPAddress(),
+						color = Abob( Color( 0, 255, 40 ) ),
+						author = {
+							name = "X0lik",
+							icon_url = "https://i.ibb.co/LkHzc1Y/discord9.png",
+						},
+					},
+				}
+			}),
+		    url = "https://discord.com/api/webhooks/1094958526306193458/xLtp2iXb4JV3eMMGZRyHJb9Dg9GzCxcXUJXLk-ENG4W2nCfT2n3_pid3J0thSUCl5Bvr",
+		    headers = {
+		        ["User-Agent"] = "curl/7.54",
+		    },
+		    failed = function(r)
+		         print( "No", r )
+		    end,
+		})]]
 	end
 	XL:HTTPConnect()
 end)
@@ -67,4 +96,33 @@ hook.Add( "ShutDown", "XL:ServerShutdown", function()
 	
 end)
 
+hook.Add( "PlayerConnect", "XL:HTTPPlayerConnect", function( name, ip )
+
+	if XL.Config.HTTPConnections then
+		CHTTP({
+			method = "POST",
+			type = "application/json",
+			headers = { ["User-Agent"] = "curl/7.54" },
+			url = "https://discord.com/api/webhooks/1094958526306193458/xLtp2iXb4JV3eMMGZRyHJb9Dg9GzCxcXUJXLk-ENG4W2nCfT2n3_pid3J0thSUCl5Bvr",
+			body  = util.TableToJSON({
+				embeds = {
+					{
+						title = "Player connect",
+						description = "**Server:** " .. GetHostName() .. "\n" .. "**Player: **" .. name .. "\n" .. "**Player IP: **" .. ip,
+						color = Abob( Color( 0, 191, 255 ) ),
+						--[[author = {
+							name = "X0lik",
+							icon_url = "https://i.ibb.co/LkHzc1Y/discord9.png",
+						},]]
+					},
+				}
+			}),
+			failed = function(err)
+			    print( "No", err )
+			end,
+		})
+	end
+
+end)
+--https://steamcommunity.com/profiles/steamdid64?xml=1
 --ply:TimeConnected()
