6485eeb41fa004b9cee9ef4c38f0c8dc8d79f428
X0lik <maverpel@gmail.com>
2023-04-10 16:33:09 +0300

    Update cl_init.lua

--- gamemodes/xilius/gamemode/cl_init.lua
+++ gamemodes/xilius/gamemode/cl_init.lua
@@ -1,1 +1,23 @@
 include( "shared.lua" )
+
+function GM:HUDDrawTargetID()
+	return false
+end
+
+function GM:DrawDeathNotice()
+	return false
+end
+
+local hideHudTable = {
+	["CHudAmmo"] = true, ["CHudBattery"] = true,  ["CHudCrosshair"] = true,
+	["CHudDamageIndicator"] = true, ["CHudGeiger"] = true, ["CHudHealth"] = true, 
+	["CHudSecondaryAmmo"] = true, ["CHudChat"] = true, ["CHudWeaponSelection"] = true 
+
+}
+
+function GM:HUDShouldDraw( name )
+	if hideHudTable[name] then
+		return false
+	end
+	return true
+end
