0e7493ce8935f17b427593ac6e1fa74dcbd20f14
X0lik <maverpel@gmail.com>
2023-04-12 18:04:47 +0300

    Fix first view bug

--- gamemodes/xilius/lib/base/player/cl_firstview.lua
+++ gamemodes/xilius/lib/base/player/cl_firstview.lua
@@ -1,9 +1,12 @@
+local isValid, curTime = IsValid, CurTime
 local firstVector, secondVector = Vector( 0, 0, 0 ), Vector( 1, 1, 1 )
 local ply, bone
 local delay = 0
-hook.Add( "Think", "XL:FirstView", function() 
 
-	if delay < CurTime() then
+local hookAdd = hook.Add
+hookAdd( "Think", "XL:FirstView", function() 
+
+	if delay < curTime() then
 		
 		ply = LocalPlayer()
 		bone = ply:LookupBone( "ValveBiped.Bip01_Head1" )
@@ -14,18 +17,18 @@ hook.Add( "Think", "XL:FirstView", function()
 				ply:ManipulateBoneScale( bone, secondVector )
 			end
 		end
-		delay = CurTime() + 0.1
+		delay = curTime() + 0.1
 	end
 
 end)
 
 local view = {}
 local attachment
-hook.Add( "CalcView", "XL:ChangeView", function( ply, _, ang, fov ) 
+hookAdd( "CalcView", "XL:ChangeView", function( ply, _, ang, fov ) 
 
-	if ply:IsValid() && ply:Alive() then
+	if isValid(ply) && ply:Alive() then
 		
-		attachment = ply:GetAttachment(1)
+		attachment = ply:GetAttachment( ply:LookupAttachment("eyes") )
 		view.origin = attachment.Pos
 		view.angles = ang
 		view.fov = fov
@@ -35,16 +38,17 @@ hook.Add( "CalcView", "XL:ChangeView", function( ply, _, ang, fov )
 
 end)
 
+local setColor, drawCircle = surface.SetDrawColor, draw.Circle
 local hitpos
-hook.Add( "HUDPaint", "XL:Crosshair", function()
+hookAdd( "HUDPaint", "XL:Crosshair", function()
 
 	ply = LocalPlayer()
 	hitpos = ply:GetEyeTrace().HitPos:ToScreen()
 
-	surface.SetDrawColor( 0, 0, 0 )
-	draw.Circle( hitpos.x, hitpos.y, 4, 10 )
-	surface.SetDrawColor( 255, 255, 255 )
-	draw.Circle( hitpos.x, hitpos.y, 3, 10 )
+	setColor( 0, 0, 0 )
+	drawCircle( hitpos.x, hitpos.y, 4, 10 )
+	setColor( 255, 255, 255 )
+	drawCircle( hitpos.x, hitpos.y, 3, 10 )
 
 end)
-hook.Add( "ShouldDrawLocalPlayer", "XL:DrawLocalPlayer", function() return true end)
+hookAdd( "ShouldDrawLocalPlayer", "XL:DrawLocalPlayer", function() return true end)
