9a8878e9e42cd71d0b6dd5848063c89902dea590
X0lik <maverpel@gmail.com>
2023-07-19 02:42:34 +0300

    Fix player initializating

--- gamemodes/xilius/lib/base/player/sv_player_initializating.lua
+++ gamemodes/xilius/lib/base/player/sv_player_initializating.lua
@@ -2,13 +2,13 @@ hook.Add( "PlayerInitialSpawn", "XL:SetDefaultTeam", function( ply )
 	timer.Simple( 0, function()
 
 		ply.NetTimedOut = {}
-		XL:SetTeam( ply, XL.DefaultTeam, 1 )
 		ply:SetCrouchedWalkSpeed( XL.Config.DefaultCrouch )
 		ply:SetSlowWalkSpeed( XL.Config.DefaultSlowWalk )
 		ply:SetWalkSpeed( XL.Config.DefaultWalk )
 		ply:SetRunSpeed( XL.Config.DefaultRun )
 		ply:SetMaxSpeed( XL.Config.DefaultRun )
 		ply:SetJumpPower( XL.Config.DefaultJump )
+		XL:SetTeam( ply, XL.DefaultTeam )
 		
 		if XL.Modules["starwars"] then
 			if not ply:CharLoad() then
